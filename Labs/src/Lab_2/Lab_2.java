e1caba69e1f1f0333dc2b24968ed478d23d8f93f
TT2PF <jaqoob0411@gmail.com>
2025-11-15 01:45:01 +0300

    add lab2

---
+++ Labs/src/Lab_2/Lab_2.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Lab_2;
+
+/**
+ *
+ * @author T.Y
+ */
+public class Lab_2 {
+    public static void main(String[] args) {
+        
+    }
+}
